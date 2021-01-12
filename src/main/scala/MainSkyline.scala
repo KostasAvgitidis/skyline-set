fda509979b0eecdf3602d1c5ea0c4ba7ae4bb3e6
KostasAvgitidis <kwnnax@gmail.com>
2021-01-12 03:31:31 +0200

    SFS skyline v2

--- src/main/scala/MainSkyline.scala
+++ src/main/scala/MainSkyline.scala
@@ -1,9 +1,11 @@
 package com.avgitidis.spark
 
 import org.apache.log4j._
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{DoubleType, StructType}
+import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
+
 import scala.collection.mutable.ArrayBuffer
 
 object MainSkyline {
@@ -12,13 +14,22 @@ object MainSkyline {
   /** Our main function where the action happens */
   def main(args: Array[String]) {
 
-    val skylines = ArrayBuffer[Row]()
-    val kDomSkylines = ArrayBuffer[Row]()
-    val nodesBuffer = ArrayBuffer[Row]()
-    val kDominant = ArrayBuffer[Row]()
-    //    val k: Int = args(0).toInt
+    //    val kDomSkylines = ArrayBuffer[Row]()
+    //    val nodesBuffer = ArrayBuffer[Row]()
+    //    val kDominant = ArrayBuffer[Row]()
+
+    //We can implement an args parser in case we compile it into a jar
     val k: Int = 10
-    val cols = List("Nodes Dominating", "A", "B", "C", "D")
+    val dims: Int = 10
+    val size: String = "10K"
+    val distribution: String = "chisquare"
+
+    val domscore: String = "DominanceScore"
+    val countstr: String = "(count - 1)"
+    var skylines = ArrayBuffer[Row]()
+    val cols = List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
+    val columns = cols.slice(0, dims)
+    val cols_ = columns :+ "Id"
     val sep = (1 to 50).map(_ => "_").mkString("_")
     val start = System.nanoTime
 
@@ -29,47 +40,33 @@ object MainSkyline {
     val spark = SparkSession
       .builder
       .appName("MySkyline")
-      .config("spark.executor.cores", 1)
       .config("spark.scheduler.pool", "default")
       .config("spark.scheduler.pool.schedulingMode", "FAIR")
       .config("spark.scheduler.mode", "FAIR")
       .config("spark.scheduler.revive.interval", "0s")
-      .config("spark.task.cpus", 2)
       .config("spark.reducer.maxSizeInFlight", "400m")
-      //            .config("spark.executor.instances", 16)
+      .config("spark.executor.memory", "24g")
       .config("spark.dynamicAllocation.enabled", "true")
-      //      .config("spark.cores.max", 8)
+      //      .config("spark.task.cpus", 2)
       .master("local[*]")
       .getOrCreate()
 
-    val pointsSchema = new StructType()
-      .add("A", DoubleType, nullable = true)
-      .add("B", DoubleType, nullable = true)
-      .add("C", DoubleType, nullable = true)
-      .add("D", DoubleType, nullable = true)
-    //      .add("E", DoubleType, nullable = true)
-    //      .add("F", DoubleType, nullable = true)
-    //      .add("G", DoubleType, nullable = true)
-    //      .add("H", DoubleType, nullable = true)
-    //      .add("I", DoubleType, nullable = true)
-    //      .add("J", DoubleType, nullable = true)
-    //    val filePath = "yourFilePath"
-
+    val nodeSchema = StructType(columns.map(n => StructField(n, DoubleType, nullable = true)))
+    val encoder = RowEncoder(nodeSchema)
     import spark.implicits._
-
     println(sep)
     println("Reading the csv file")
 
     val nodes = spark.read
       .option("header", "true")
-      .schema(pointsSchema)
-      //      .csv("data/1Ms_10Fdata.csv")
-      .csv("data/10K_4Fdata.csv")
-      //      .csv(filePath)
-      .withColumn("Sum", SkylineFunctions.getSumUDF(col("A"), col("B"), col("C"), col("D")))
-      //      .withColumn("Sum",getSumUDF(col("A"),col("B"),col("C"),col("D"),col("E"),col("F"),col("G"),col("H"),col("I"),col("J")))
+      .schema(nodeSchema)
+      .csv(s"data/${size}x${dims}_$distribution.csv")
+      .withColumn("Sum", SkylineFunctions.getSumUDF(struct(columns.map(col): _*)))
+      .withColumn("Id", monotonically_increasing_id())
       .sort($"Sum")
       .drop($"Sum")
+      .as("nodes")
+      .toDF
       .cache()
 
     //  Task 1
@@ -77,62 +74,204 @@ object MainSkyline {
     println("Starting Task1")
     println("Calculating Skyline Points")
 
-    nodes.collect.foreach(x => SkylineFunctions.sfsSkyline(skylines, Iterator(x))
-    )
+
+    nodes.collect.par.foreach(x => SkylineFunctions.sfsSkyline(skylines, Iterator(x)))
+    nodes.collect.par.foreach(x => SkylineFunctions.onepctSkylines(skylines, Iterator(x)))
+    skylines = skylines.distinct
 
     println("Found " + skylines.length + " Skyline Points")
-    println("Task1 finished in: " + BigDecimal((System.nanoTime - t1) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toFloat + " seconds")
+    val t1_ = BigDecimal((System.nanoTime - t1) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+    println("Task1 finished in: " + t1_ + " seconds")
+
+    //           skylines.foreach(f=>{
+    //               println(f)
+    //           })
     println(sep)
-    //       skylines.foreach(f=>{
-    //           println(f)
-    //       })
+
+    //     Task 3
+    val t3 = System.nanoTime
+    println("Starting Task3")
+
+    var skylinesdf = spark.emptyDataFrame
+    dims match {
+      case 2 =>
+        skylinesdf = skylines.par
+          .map({
+            case Row(val1: Double, val2: Double, id: Long) => (val1, val2, id)
+          }
+          )
+          .toList
+          .toDF(cols_ : _*)
+          .as("skylines")
+      case 5 =>
+        skylinesdf = skylines.par
+          .map({
+            case Row(val1: Double, val2: Double, val3: Double, val4: Double, val5: Double, id: Long) => (val1, val2, val3, val4, val5, id)
+          }
+          )
+          .toList
+          .toDF(cols_ : _*)
+          .as("skylines")
+      case 10 =>
+        skylinesdf = skylines.par
+          .map({
+            case Row(val1: Double, val2: Double, val3: Double, val4: Double, val5: Double, val6: Double, val7: Double, val8: Double, val9: Double, val10: Double, id: Long) => (val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, id)
+          }
+          )
+          .toList
+          .toDF(cols_ : _*)
+          .as("skylines")
+    }
+
+    val jdf = nodes.crossJoin(skylinesdf)
+    var kdom_skyline = spark.emptyDataFrame
+
+    dims match {
+      case 2 =>
+        kdom_skyline = jdf.filter(col("skylines.A") <= col("nodes.A") &&
+          col("skylines.B") <= col("nodes.B"))
+          .groupBy("skylines.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("kskylines")
+          .join(skylinesdf, $"skylines.Id" === $"kskylines.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+      case 5 =>
+        kdom_skyline = jdf.filter(col("skylines.A") <= col("nodes.A") &&
+          col("skylines.B") <= col("nodes.B") &&
+          col("skylines.C") <= col("nodes.C") &&
+          col("skylines.D") <= col("nodes.D") &&
+          col("skylines.E") <= col("nodes.E"))
+          .groupBy("skylines.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("kskylines")
+          .join(skylinesdf, $"skylines.Id" === $"kskylines.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+      case 10 =>
+        kdom_skyline = jdf.filter(col("skylines.A") <= col("nodes.A") &&
+          col("skylines.B") <= col("nodes.B") &&
+          col("skylines.C") <= col("nodes.C") &&
+          col("skylines.D") <= col("nodes.D") &&
+          col("skylines.E") <= col("nodes.E") &&
+          col("skylines.F") <= col("nodes.F") &&
+          col("skylines.G") <= col("nodes.G") &&
+          col("skylines.H") <= col("nodes.H") &&
+          col("skylines.I") <= col("nodes.I") &&
+          col("skylines.J") <= col("nodes.J"))
+          .groupBy("skylines.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("kskylines")
+          .join(skylinesdf, $"skylines.Id" === $"kskylines.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+    }
+
+    kdom_skyline.show(k)
+    //
+    //    nodes.collect.foreach(x => SkylineFunctions.kDominateCount(kDomSkylines, skylines, Iterator(x))
+    //    )
+    //
+    //    val kdf_ = kDomSkylines.groupBy(identity).mapValues(_.size).par.map(_.swap)
+    //      .map({
+    //        case (x: Int, Row(val1: Double, val2: Double,id:Long)) => (x, val1, val2,id)
+    //            case Row(val1: Double, val2: Double, val3: Double, val4: Double,val5: Double,id:Long) => (val1, val2, val3, val4, val5,id)
+    //        case (x: Int, Row(val1: Double, val2: Double, val3: Double, val4: Double, val5: Double, val6: Double, val7: Double, val8: Double, val9: Double, val10: Double,id:Long)) => (x, val1, val2, val3, val4, val5,val6,val7,val8,val9,val10,id)
+    //      }
+    //      )
+    //      .toList
+    //      .toDF(cols: _*)
+    //      .sort(desc("Nodes Dominating"))
+    //    println("The " + k + " Most Dominant Skyline Nodes")
+    //    kdf_.show(k)
+    val t3_ = BigDecimal((System.nanoTime - t3) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+    println("Task3 finished in: " + t3_ + " seconds")
+    println(sep)
 
     //Task 2
     val t2 = System.nanoTime
     println("Starting Task2")
 
-    nodes.collect.foreach(x => SkylineFunctions.dfToIterator(nodesBuffer, Iterator(x))
-    )
-    nodes.collect.foreach(x => SkylineFunctions.kDominateCount(kDominant, nodesBuffer, Iterator(x))
-    )
-    val kDf2 = kDominant.groupBy(identity).mapValues(_.size).toList.map(_.swap)
-      .map({
-        case (x: Int, Row(val1: Double, val2: Double, val3: Double, val4: Double)) => (x, val1, val2, val3, val4)
-      }
-      )
-      .toDF(cols: _*)
-      .sort(desc("Nodes Dominating"))
+    val jdf2 = nodes.as("nodes").crossJoin(nodes.as("nodes1")).join(skylinesdf, $"nodes1.Id" =!= "skylines.Id", "left_anti")
+    jdf2.head()
+    var kdom_all = spark.emptyDataFrame
 
-    println("The " + k + " Most Dominant Nodes")
-    kDf2.show(k)
-    println("Task2 finished in: " + BigDecimal((System.nanoTime - t2) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toFloat + " seconds")
+    dims match {
+      case 2 =>
+        kdom_all = jdf2.filter(col("nodes.A") <= col("nodes1.A") &&
+          col("nodes.B") <= col("nodes1.B"))
+          .groupBy("nodes.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("fnodes")
+          .join(nodes, $"fnodes.Id" === $"nodes.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+      case 5 =>
+        kdom_all = jdf2.filter(col("nodes.A") <= col("nodes1.A") &&
+          col("nodes.B") <= col("nodes1.B") &&
+          col("nodes.C") <= col("nodes1.C") &&
+          col("nodes.D") <= col("nodes1.D") &&
+          col("nodes.E") <= col("nodes1.E"))
+          .groupBy("nodes.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("fnodes")
+          .join(nodes, $"fnodes.Id" === $"nodes.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+      case 10 =>
+        kdom_all = jdf2.filter(col("nodes.A") <= col("nodes1.A") &&
+          col("nodes.B") <= col("nodes1.B") &&
+          col("nodes.C") <= col("nodes1.C") &&
+          col("nodes.D") <= col("nodes1.D") &&
+          col("nodes.E") <= col("nodes1.E") &&
+          col("nodes.F") <= col("nodes1.F") &&
+          col("nodes.G") <= col("nodes1.G") &&
+          col("nodes.H") <= col("nodes1.H") &&
+          col("nodes.I") <= col("nodes1.I") &&
+          col("nodes.J") <= col("nodes1.J"))
+          .groupBy("nodes.Id")
+          .count()
+          .select($"id", $"count" - 1).withColumnRenamed(countstr, domscore)
+          .as("fnodes")
+          .join(nodes, $"fnodes.Id" === $"nodes.Id")
+          .drop("Id")
+          .sort(desc(domscore))
+    }
+
+    kdom_all.union(skylinesdf).sort(desc(domscore))
+    kdom_all.show(k)
+
+    //    nodes.collect.foreach(x => SkylineFunctions.dfToIterator(nodesBuffer, Iterator(x))
+    //    )
+    //    nodes.collect.foreach(x => SkylineFunctions.kDominateCount(kDominant, nodesBuffer, Iterator(x))
+    //    )
+    //    val kdf = kDominant.groupBy(identity).mapValues(_.size).par.map(_.swap)
+    //      .map({
+    //            case (x: Int, Row(val1: Double, val2: Double,id:Long)) => (x, val1, val2,id)
+    //            case Row(val1: Double, val2: Double, val3: Double, val4: Double,val5: Double,id:Long) => (val1, val2, val3, val4, val5,id)
+    //            case (x: Int, Row(val1: Double, val2: Double, val3: Double, val4: Double, val5: Double, val6: Double, val7: Double, val8: Double, val9: Double, val10: Double,id:Long)) => (x, val1, val2, val3, val4, val5,val6,val7,val8,val9,val10,id)
+    //      }
+    //      )
+    //      .toList
+    //      .toDF(cols: _*)
+    //      .sort(desc("Nodes Dominating"))
+    //
+    //    println("The " + k + " Most Dominant Nodes")
+    //    kdf.show(k)
+    val t2_ = BigDecimal((System.nanoTime - t2) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+    println("Task2 finished in: " + t2_ + " seconds")
     println(sep)
 
-    // Task 3
-    val t3 = System.nanoTime
-    println("Starting Task3")
-
-    nodes.collect.foreach(x => SkylineFunctions.kDominateCount(kDomSkylines, skylines, Iterator(x))
-    )
-
-    val kDf = kDomSkylines.groupBy(identity).mapValues(_.size).toList.map(_.swap)
-      .map({
-        case (x: Int, Row(val1: Double, val2: Double, val3: Double, val4: Double)) => (x, val1, val2, val3, val4)
-      }
-      )
-      .toDF(cols: _*)
-      .sort(desc("Nodes Dominating"))
-    println("The " + k + " Most Dominant Skyline Nodes")
-    kDf.show(k)
-    println("Task3 finished in: " + BigDecimal((System.nanoTime - t3) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toFloat + " seconds")
-    println(sep)
 
     spark.stop()
-    println("All requested tasks finished in " + BigDecimal((System.nanoTime - start) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toFloat + " seconds")
+    val duration = BigDecimal((System.nanoTime - start) / 1e9d).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+    println("All requested tasks finished in " + duration + " seconds")
     println(sep)
+    //    println(s"$duration $t1_ $t2_ $t3_")
   }
 }
-
-
-
-
--- src/main/scala/SkylineFunctions.scala
+++ src/main/scala/SkylineFunctions.scala
@@ -3,86 +3,122 @@ package com.avgitidis.spark
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
+
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks.{break, breakable}
 
 object SkylineFunctions {
   //Simple Sum UDF
-  val getSum: (Float, Float, Float, Float) => Float = (a: Float, b: Float, c: Float, d: Float) => {
-    //  val getSum : (Float,Float,Float,Float,Float,Float,Float,Float,Float,Float) => Float = (a:Float,b:Float,c:Float,d:Float,e:Float,f:Float,g:Float,h:Float,i:Float,j:Float)=>{
-    //    a + b + c + d + e + f + g + h + i + j
-    a + b + c + d
+  val getSum: Row => Double = (row: Row) => {
+    var sum: Double = 0
+    val rows = row.toSeq
+    for (i <- rows.indices) {
+      sum += rows(i).toString.toDouble
+    }
+    sum
   }
-
   val getSumUDF: UserDefinedFunction = udf(getSum)
-
-  //Point A -Dominates-> Point B
-  def aDominatesb(x: Row, y: Row): Boolean = {
-    val features = x.length
-    for (i <- 0 until features) {
-      if (x(i).toString.toDouble > y(i).toString.toDouble) {
-        return false
-      }
-    }
-    for (j <- 0 until features) {
-      if (x(j).toString.toDouble < y(j).toString.toDouble) {
-        return true
-      }
-    }
-    false
-  }
 
   //Sort Filter Skyline Algorithm
   def sfsSkyline(skylines: ArrayBuffer[Row], rowIterator: Iterator[Row]): Iterator[Row] = {
     var initial = false
-    val rowArray = rowIterator.toArray
+    val row = rowIterator.toList.head
     if (skylines.isEmpty) {
-      skylines += rowArray(0)
+      skylines += row
       initial = true
     }
-    for (i <- rowArray.indices) {
-      var j = 0
-      var stop = false
-      breakable {
-        while (j < skylines.length) {
-
-          if (aDominatesb(rowArray(i), skylines(j))) {
-            skylines.remove(j)
+    var j = 0
+    var stop = false
+    breakable {
+      while (skylines.length > j) {
+        try {
+          if (aDominatesb(row, skylines(j))) {
+            skylines -= skylines(j)
+            stop = false
             j -= 1
           }
-          else if (aDominatesb(skylines(j), rowArray(i))) {
+          else if (aDominatesb(skylines(j), row)) {
             stop = true
             break()
           }
-          if (initial & i == 0) {
-            stop = true
-            break()
-          }
-          j += 1
         }
+        catch {
+          case e: IndexOutOfBoundsException =>
+            stop = true
+            break()
+        }
+        if (initial) {
+          stop = true
+          break()
+        }
+        j += 1
       }
       if (!stop) {
-        skylines += rowArray(i)
+        skylines += row
       }
     }
-    skylines.toIterator
+    rowIterator
   }
 
-  //Converting a Dataframe into an Iterator[Row]
+  //Point A -Dominates-> Point B
+  def aDominatesb(x: Row, y: Row): Boolean = {
+    try {
+      val features = x.length
+      for (i <- 0 until features - 1) {
+        if (x(i).toString.toDouble > y(i).toString.toDouble) {
+          return false
+        }
+      }
+      for (j <- 0 until features - 1) {
+        if (x(j).toString.toDouble < y(j).toString.toDouble) {
+          return true
+        }
+      }
+    }
+    catch {
+      case e: NullPointerException => return true
+    }
+    false
+  }
+
+  //0.1% Skylines left
+  def onepctSkylines(skylines: ArrayBuffer[Row], rowIterator: Iterator[Row]): Iterator[Row] = {
+    val row = rowIterator.toList.head
+    var j = 0
+    var stop = false
+    breakable {
+      while (skylines.length > j) {
+        if (aDominatesb(row, skylines(j))) {
+          skylines -= skylines(j)
+          stop = false
+          j -= 1
+        }
+        else if (aDominatesb(skylines(j), row)) {
+          stop = true
+          break()
+        }
+        j += 1
+      }
+    }
+    if (!stop) {
+      skylines += row
+    }
+    rowIterator
+  }
+
+  //Converting a Dataframe into an Iterator[Row] (Task 3 alternative version)
   def dfToIterator(buffer: ArrayBuffer[Row], rowIterator: Iterator[Row]): Iterator[Row] = {
-    val row = rowIterator.toArray
-    buffer += row(0)
+    val row = rowIterator.toList.head
+    buffer += row
     buffer.toIterator
   }
 
-  //Get k-Dominating points
+  //Get k-Dominating points (Task2 and 3 alternative version)
   def kDominateCount(buffer: ArrayBuffer[Row], skylines: ArrayBuffer[Row], it: Iterator[Row]): ArrayBuffer[Row] = {
-    val rowArray = it.toArray
-    for (i <- rowArray.indices) {
-      for (j <- skylines.indices) {
-        if (aDominatesb(skylines(j), rowArray(i))) {
-          buffer += skylines(j)
-        }
+    val row = it.toList.head
+    for (j <- skylines.indices) {
+      if (aDominatesb(skylines(j), row)) {
+        buffer += skylines(j)
       }
     }
     buffer
